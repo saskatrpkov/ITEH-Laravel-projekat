b496735ddfbc84bf2b678d1f5186dcc0ef2eb5cb
saskatrpkov <aleksandratrpkov1@gmail.com>
2022-01-09 02:11:34 +0100

    Kreiran model sastojak

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Food extends Model
+class Ingredient extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'description', 'vegan', 'gluten_free', 'prep_time'];
+    protected $fillable = ['name', 'vegan', 'gluten_free'];
 }
